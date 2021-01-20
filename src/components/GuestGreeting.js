f804b856b43bf800ff91cd070115450ff6f8e50c
Jennifer Smith <59855990+JenniferSmith007@users.noreply.github.com>
2021-01-19 21:04:13 -0500

    Conditional Rendering

---
+++ src/components/GuestGreeting.js
@@ -0,0 +1,5 @@
+function GuestGreeting(props) {
+  return <h1>Please sign Up!</h1>;
+}
+
+export default GuestGreeting;
---
+++ src/components/UserGreeting.js
@@ -0,0 +1,5 @@
+function UserGreeting(props) {
+  return <h1>Hello, welcome back</h1>;
+}
+
+export default UserGreeting;
